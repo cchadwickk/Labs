f8ae5b805f1d9d6ecafa26058d8bac6de9d807a0
cchadwickk <chaitanyakadian@gmail.com>
2017-02-01 12:22:24 +0530

    Create demorgan1.m

---
+++ SC/2/demorgan1.m
@@ -0,0 +1,23 @@
+close all;
+clear all;
+clc;
+x=(0:0.1:10);
+A=gaussmf(x, [1 4]);
+B=trimf(x, [3 6.5 9]);
+ANot=1-A;
+BNot=1-B;
+AIntB=min(A,B);
+subplot(3,1,1);
+hold on
+plot(x,A,'r');
+plot(x,B,'g');
+axis([0 10 0 1.05]);
+legend('A','B');
+subplot(3,1,2);
+plot(x,(1-AIntB),'r');
+axis([0 10 0 1.05]);
+legend('not(A∩B)')
+subplot(3,1,3);
+plot(x,max(ANot,BNot),'r');
+axis([0 10 0 1.05]);
+legend('not(A)U not(B)')
